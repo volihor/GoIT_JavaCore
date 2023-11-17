487ad69bd7f7ceb3d202d32bfad5920335c560fe
volihor <you@example.com>
2023-11-17 22:13:59 +0200

    HW, module 10 ( 3. Changed class WordsCounter.)

--- src/main/java/mod_10_IOStreams/WordsCounter.java
+++ src/main/java/mod_10_IOStreams/WordsCounter.java
@@ -9,11 +9,14 @@ import java.util.stream.Stream;
 public class WordsCounter {
 
     public static void main(String[] args) {
+
         String countWords = "";
+        // read from words.txt file to string countWords
         try {
             FileReader fr = new FileReader("src/main/java/mod_10_IOStreams/words.txt");
             Scanner sc = new Scanner(fr);
             String strWords = "";
+
             while (sc.hasNext()){
                 strWords += " " + sc.nextLine();
             }
@@ -23,21 +26,54 @@ public class WordsCounter {
             e.printStackTrace();
         }
 
-        List<String> listWords = Arrays.asList(countWords);
-        Map<String, Integer> wordCounts = Stream.of(listWords)
-                .flatMap(Collection::stream)
-                .flatMap(str -> Arrays.stream(str.split(" ")))
-                .collect(Collectors.toMap(
-                        String::toLowerCase,
-                        i -> 1,
-                        Integer::sum)
-                );
+        // save unique world to the HashMap<String, Integer> from ArrayList<String> listWords
+        // with value as the number of counted words
+        ArrayList<String> listWords = new ArrayList<>( Arrays.asList(countWords.split(" ")) );
+        HashMap<String, Integer> wordCountsMap = new HashMap<>();
+//            System.out.println("listWords = " + listWords);
+        for(String string : listWords){
+            wordCountsMap.put( string,
+                    (wordCountsMap.containsKey(string) ? wordCountsMap.get(string)+1 : 1 )
+            );
+        }
+//        wordCountsMap.entrySet().stream()
+//                .forEach(System.out::println);
 
-        System.out.println(wordCounts);
-        for(String key: wordCounts.keySet()){
-            System.out.println(key + " " + wordCounts.get(key));
+        // Sort the ArrayList
+
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Integer> > list =
+                new LinkedList<Map.Entry<String, Integer> >(wordCountsMap.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
+            public int compare(Map.Entry<String, Integer> m1,
+                               Map.Entry<String, Integer> m2)
+            {
+                return (m2.getValue()).compareTo(m1.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<String, Integer> sortedWordCountsMap = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> map : list) {
+            sortedWordCountsMap.put(map.getKey(), map.getValue());
         }
 
+//        sortedWordCountsMap.entrySet().forEach(System.out::println);
+
+        for(String key: sortedWordCountsMap.keySet()){
+            System.out.println(key + " " + wordCountsMap.get(key));
+        }
+
+
+//        HashMap<String, Integer> sortedWordCountsMap = wordCountsMap.entrySet().stream()
+//                .sorted((es1, es2) -> es2.getValue().compareTo(es1.getValue()))
+//                .collect(Collectors.toMap(
+//                      String::toLowerCase,
+//                      i -> 1,
+//                      Integer::sum)
+//                );
 
     }
 }
